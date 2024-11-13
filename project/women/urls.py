7cb0ab89f7dd6b6ff296624886c1a29a073e23c4
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2024-11-13 19:49:20 +0200

    register new views with path params and regexp patterns

--- project/women/urls.py
+++ project/women/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("cats/", views.categories, name="categories"),
+    path("cats/<int:cat_id>/", views.category_by_id, name="category_by_id"),
+    path("cats/<slug:cat_slug>/", views.category_by_slug, name="category_by_slug"),
+    re_path("^archive/cats/(?P<year>[0-9]{4})/", views.categories_archive, name="categories_archive"),
 ]
--- project/women/views.py
+++ project/women/views.py
@@ -8,3 +8,15 @@ def index(request):
 
 def categories(request):
     return HttpResponse("<h1>Categories page</h1>")
+
+
+def category_by_id(request, cat_id):
+    return HttpResponse(f"<h1>Category page</h1><p>id: {cat_id}</p>")
+
+
+def category_by_slug(request, cat_slug):
+    return HttpResponse(f"<h1>Category page</h1><p>slug: {cat_slug}</p>")
+
+
+def categories_archive(request, year):
+    return HttpResponse(f"<h1>Categories archieve page</h1><p>year: {year}</p>")
