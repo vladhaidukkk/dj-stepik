2a9617983cb9d3cb1ba82a8ff4a3f51a2e62280c
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2024-11-13 19:55:05 +0200

    replace url regexp pattern with custom converter

---
+++ project/women/converters.py
@@ -0,0 +1,8 @@
+class FourDigitYearConverter:
+    regex = r"[0-9]{4}"
+
+    def to_python(self, value):
+        return int(value)
+
+    def to_url(self, value):
+        return f"{value:04d}"
--- project/women/urls.py
+++ project/women/urls.py
@@ -1,11 +1,13 @@
-from django.urls import path, re_path
+from django.urls import path, register_converter
 
-from . import views
+from . import converters, views
+
+register_converter(converters.FourDigitYearConverter, "yyyy")
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("cats/", views.categories, name="categories"),
     path("cats/<int:cat_id>/", views.category_by_id, name="category_by_id"),
     path("cats/<slug:cat_slug>/", views.category_by_slug, name="category_by_slug"),
-    re_path("^archive/cats/(?P<year>[0-9]{4})/", views.categories_archive, name="categories_archive"),
+    path("archive/cats/<yyyy:year>/", views.categories_archive, name="categories_archive"),
 ]
