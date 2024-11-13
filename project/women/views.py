305bac60a5f17f4b507ee16dc3bee7b8e2b65a63
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2024-11-13 18:03:14 +0200

    create basic views in the app and make them available

--- project/women/views.py
+++ project/women/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("<h1>Index page</h1>")
+
+
+def categories(request):
+    return HttpResponse("<h1>Categories page</h1>")
